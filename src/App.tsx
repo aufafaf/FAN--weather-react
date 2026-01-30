9499766f2ead8ba2e5145cbbacb1eaf07712e050
aufafaf <mfaufalahatx@gmail.com>
2026-01-30 10:24:31 +0700

    add api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface WeatherData {
+  temp: number;
 }
 
-export default App
+function App() {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  const [city, setCity] = useState("");
+  const [data, setData] = useState<WeatherData | null>(null);
+
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`,
+      );
+
+      if (!response.ok) {
+        throw new Error("Kota tidak ditemukan");
+      }
+
+      const result = await response.json();
+      setData(result.main);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+      } else {
+        console.error("Terjadi error yang tidak diketahui");
+      }
+    }
+  };
+  return (
+    <div className="p-10 flex justify-center items-center min-h-screen gap-5">
+      <input onChange={(e) => setCity(e.target.value)} className="border p-2" />
+      <button
+        onClick={fetchWeather}
+        className="border p-2 rounded-full py-1 px-3 bg-amber-500 hover:bg-amber-300"
+      >
+        Search
+      </button>
+
+      {data && <p>Temperature : {data.temp}^C</p>}
+    </div>
+  );
+}
+
+export default App;
